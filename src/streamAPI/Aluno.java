d7b85443318072d81889d343835549347ee6e712
Matheus Brito Biserra <matheus.biserra@projecao.br>
2022-02-14 13:54:18 -0300

    Teste Imprimir Notas

--- src/streamAPI/Aluno.java
+++ src/streamAPI/Aluno.java
@@ -3,13 +3,26 @@ package streamAPI;
 public class Aluno {
 	
 	String nome;
-	double nota;
+	double nota1;
+	double nota2;
 	
-	public Aluno(String nome, double nota) {
+	public Aluno(String nome, double nota1, double nota2) {
 		this.nome = nome;
-		this.nota = nota;
+		this.nota1 = nota1;
+		this.nota2 = nota2;
+		
 	}
 	
+	double calcularMedia() {
+		double media = 0;
+		media = (nota1+nota2)/2;
+			
+		return media;
+	}
 	
+	String imprimirResultado() {
+		String resultado = String.format("%s - %.1f - ", nome, calcularMedia());
+		return resultado ;
+	}
 
 }
--- src/streamAPI/AlunoTeste.java
+++ src/streamAPI/AlunoTeste.java
@@ -2,7 +2,8 @@ package streamAPI;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.function.Predicate;
+import java.util.function.Consumer;
+import java.util.function.Function;
 
 public class AlunoTeste {
 	
@@ -13,19 +14,26 @@ public class AlunoTeste {
 		//Calcular a media das notas finais
 		
 		List<Aluno> alunos = new ArrayList<>();
-		alunos.add(new Aluno("Ana", 8.5));
-		alunos.add(new Aluno("Amanda", 9.1));
-		alunos.add(new Aluno("Augusto", 6.5));
-		alunos.add(new Aluno("Pedro", 8.5));
-		alunos.add(new Aluno("Liza", 7.3));
+		alunos.add(new Aluno("Ana", 8.5, 9.5));
+		alunos.add(new Aluno("Amanda", 9.1, 6.0));
+		alunos.add(new Aluno("Augusto", 6.5, 7));
+		alunos.add(new Aluno("Pedro", 8.5,5));
+		alunos.add(new Aluno("Liza", 7.3,10));
+		
+		
+		alunos.stream().
+		forEach(an -> System.out.println(an.nome +" Nota:"+ an.calcularMedia()));
+		System.out.println("\n");
 		
 		
 		//Filtar os aprovados
-		Predicate<Aluno> aprovados = al -> al.nota>=7.0;
+		//Predicate<Aluno> aprovados = al -> al.calcularMedia()>=7.0;
+		//Function<Aluno, String> msg = ap -> ap.calcularMedia()>=7.0? "Aprovado": "Reprovado";
+		Consumer<Aluno> resul = a -> a.imprimirResultado();
 		
-		alunos.stream()
-		.filter(aprovados)
-		.forEach(ap -> System.out.println(ap.nome));
+		alunos.stream().forEach(resul);
+		
+		
 		
 		
 		
