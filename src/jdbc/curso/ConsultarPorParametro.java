380b571d490e23764f1476837c450c5b4215c3d2
Matt-BB-99 <matheusb790@gmail.com>
2022-03-30 18:21:14 -0300

    Consultar por Parametro

---
+++ src/jdbc/curso/ConsultarPorParametro.java
@@ -0,0 +1,43 @@
+package jdbc.curso;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+public class ConsultarPorParametro {
+	public static void main(String[] args) throws SQLException {
+		Connection conexao = FabricaConexao.getConexao();
+		
+		Scanner e = new Scanner(System.in);
+		
+		System.out.println("Digite um valor para pesquisar:");
+		String valor = null;
+		valor = e.next();
+		
+		
+		String sql = "SELECT * FROM PESSOAS WHERE NOME LIKE ?";
+		
+		PreparedStatement stmt = conexao.prepareStatement(sql);
+		stmt.setString(1,"%" + valor +"%" );
+		ResultSet resultado = stmt.executeQuery();
+		
+		
+		List<Pessoa> listaPessoas = new ArrayList<>();
+		while(resultado.next()) {
+				int cod = resultado.getInt("COD");
+				String nome = resultado.getString("NOME");
+				listaPessoas.add(new Pessoa(cod, nome));
+		}
+		listaPessoas.stream().forEach(p -> 
+		System.out.println("Cod:"+p.getCod()+"=>"+p.getNome()));
+		
+		
+		
+		e.close();
+		
+	}
+}
--- src/jdbc/curso/FabricaConexao.java
+++ src/jdbc/curso/FabricaConexao.java
@@ -8,7 +8,7 @@ public class FabricaConexao {
 	public static Connection getConexao() {
 		try {
 			//Login do banco
-			final String conexaoBD = "jdbc:mysql://localhost/CURSO_JAVA";
+			final String conexaoBD = "jdbc:mysql://localhost/curso_java";
 			final String  usuario = "root";
 			final String senha = "314159265";
 			//realiza a conexao
